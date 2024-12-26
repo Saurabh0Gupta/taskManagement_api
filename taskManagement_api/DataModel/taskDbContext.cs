a99e3e36a552589c7e138a614c2faab6d3645c37
Jaimin Shah <jaimin.shah@techextensor.com>
2024-12-26 11:02:42 +0530

    Add project files.

---
+++ taskManagement_api/DataModel/taskDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace taskManagement_api.DataModel
+{
+    public class taskDbContext : DbContext
+    {
+        //setup db context
+        public taskDbContext(DbContextOptions<taskDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
